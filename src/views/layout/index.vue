73108da160c046aba4be36430d520ed8edb12cf2
Ezekielx <ezekielx.evans@gmail.com>
2025-09-29 20:46:07 +0800

    功能页面大体布局

--- src/views/layout/index.vue
+++ src/views/layout/index.vue
@@ -3,27 +3,29 @@
 </script>
 
 <template>
-  <div class="common-layout">
-
-    <el-container>
-      <!-- 顶部导航栏 -->
-      <el-header>
-        Header
-      </el-header>
-
-      <el-container>
-        <!-- 侧边栏 -->
-        <el-aside width="200px">
-          Aside
-        </el-aside>
-        <!-- 主体内容 -->
-        <el-main>
-          Main
-        </el-main>
-      </el-container>
-    </el-container>
-
-  </div>
+    <div class="common-layout">
+        <el-container>
+            <!-- 侧边栏 -->
+            <el-aside width="200px">
+                <el-container>
+                    <!-- Loge + Title -->
+                    <el-header>
+                        Header
+                    </el-header>
+                    <!-- Menu -->
+                    <el-main>
+                        Menu
+                    </el-main>
+                </el-container>
+            </el-aside>
+            <!-- 主页面 -->
+            <el-container>
+                <el-main>
+                    Main
+                </el-main>
+            </el-container>
+        </el-container>
+    </div>
 </template>
 
 <style scoped>
