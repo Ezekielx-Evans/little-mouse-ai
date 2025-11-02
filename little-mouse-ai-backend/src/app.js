606eee4c18248a024f9f39a5bb5ce6be9c509953
Ezekielx <ezekielx.evans@gmail.com>
2025-11-02 10:34:53 +0800

    getHello 示例接口

--- little-mouse-ai-backend/src/app.js
+++ little-mouse-ai-backend/src/app.js
@@ -1,11 +1,11 @@
 import express from 'express'
 
+import routes from './routes/index.js'
+
 const app = express()
 const port = 3000
 
-app.get('/', (req, res) => {
-    res.send('Hello World!')
-})
+app.use(routes)
 
 app.listen(port, () => {
     console.log(`后端服务器在 ${port} 端口上运行`)
--- little-mouse-ai-backend/src/routes/index.js
+++ little-mouse-ai-backend/src/routes/index.js
@@ -1,5 +1,8 @@
 import express from 'express'
+import systemRoutes from './systemRoutes.js'
 
 const router = express.Router()
+
+router.use('/system', systemRoutes)
 
 export default router
